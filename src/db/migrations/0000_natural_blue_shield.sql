dba79ca54fe18ed27333e3e243972e5a3b4aa5c0
Alla <your.allevty9@gmail.com>
2025-06-28 00:54:23 +0100

    initial commit

--- src/db/migrations/0000_natural_blue_shield.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "favorites" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"user_id" varchar,
-	"content_id" varchar,
-	"title" varchar NOT NULL,
-	"descript" text,
-	"image_url" varchar,
-	"category_id" integer NOT NULL
-);
