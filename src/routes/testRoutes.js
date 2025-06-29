6fee6e6fdd3115dae111ee28cb25942908431c99
Alla <your.allevty9@gmail.com>
2025-06-29 15:41:20 +0100

    initial commit

--- src/routes/testRoutes.js
+++ src/routes/testRoutes.js
@@ -2,6 +2,16 @@ import express from "express";
 import Test from "../models/Test.js";
 const router = express.Router();
 
+// GET all available tests
+router.get("/", async (req, res) => {
+  try {
+    const tests = await Test.find({}, "title description type");
+    res.json(tests);
+  } catch (error) {
+    res.status(500).json({ message: "Could not fetch tests" });
+  }
+});
+
 // GET a specific test
 router.get("/:id", async (req, res) => {
   try {
@@ -11,16 +21,6 @@ router.get("/:id", async (req, res) => {
   } catch (error) {
     console.error("Error fetching test:", error);
     res.status(500).json({ message: "Server error" });
-  }
-});
-
-// GET all available tests
-router.get("/alltest", async (req, res) => {
-  try {
-    const tests = await Test.find({}, "title description type");
-    res.json(tests);
-  } catch (error) {
-    res.status(500).json({ message: "Could not fetch tests" });
   }
 });
 
